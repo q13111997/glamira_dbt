903eb0f573f9de4d48da29423edf8edbc476d1ad
q13111997 <quannguyenhong22@gmail.com>
2025-12-28 22:28:16 +0700

    commit

--- models/marts/mart_glamira/dim_store.sql
+++ models/marts/mart_glamira/dim_store.sql
@@ -2,3 +2,4 @@ SELECT DISTINCT
     store_id
     ,store_name
 FROM {{ ref ('stg_glamira_store')}}
+WHERE store_id IS NOT NULL
--- models/staging/staging_glamira/stg_glamira_sales_order.sql
+++ models/staging/staging_glamira/stg_glamira_sales_order.sql
@@ -1,36 +1,32 @@
 WITH t1 AS (
     SELECT
-        SAFE_CAST(user_id_db AS INT64) AS user_id_db
-        ,LOWER(email_address) AS email_address
-        ,ip
-        ,FORMAT_DATE('%Y%m%d',DATE(TIMESTAMP_SECONDS(time_stamp))) AS date_id
-        ,CAST(store_id AS INT64) AS store_id
-        ,CAST(cp.product_id AS INT64) AS product_id
-        ,opt.option_label
-        ,opt.value_label
-        ,CAST(CAST(order_id AS FLOAT64) AS INT64) AS order_id
-        ,PARSE_TIMESTAMP('%Y-%m-%d %H:%M:%S', local_time) AS order_ts_utc
-        ,cp.currency
-        ,REGEXP_REPLACE(cp.price, r'[^\x20-\x7E]', '') AS price
-        ,CAST(cp.amount AS INT64) AS quantity
+        SAFE_CAST(user_id_db AS INT64) AS user_id_db,
+        LOWER(email_address) AS email_address,
+        ip,
+        FORMAT_DATE('%Y%m%d', DATE(TIMESTAMP_SECONDS(time_stamp))) AS date_id,
+        SAFE_CAST(store_id AS INT64) AS store_id,
+        SAFE_CAST(cp.product_id AS INT64) AS product_id,
+        SAFE_CAST(SAFE_CAST(order_id AS FLOAT64) AS INT64) AS order_id,
+        PARSE_TIMESTAMP('%Y-%m-%d %H:%M:%S', local_time) AS order_ts_utc,
+        cp.currency,
+        REGEXP_REPLACE(cp.price, r'[^\x20-\x7E]', '') AS price,
+        SAFE_CAST(cp.amount AS INT64) AS quantity
     FROM {{ source('glamira_raw', 'summary') }}
     CROSS JOIN UNNEST(cart_products) AS cp
-    LEFT JOIN UNNEST(cp.option) AS opt
     WHERE collection = 'checkout_success'
-), t2 AS (
+),
+t2 AS (
     SELECT
-        user_id_db
-        ,email_address
-        ,ip
-        ,date_id
-        ,store_id
-        ,product_id
-        ,option_label
-        ,value_label
-        ,order_id
-        ,order_ts_utc
-        ,currency
-        ,CAST(
+        user_id_db,
+        email_address,
+        ip,
+        date_id,
+        store_id,
+        product_id,
+        order_id,
+        order_ts_utc,
+        currency,
+        CAST(
             CASE 
                 WHEN TRIM(price) = '' THEN NULL 
                 WHEN REGEXP_CONTAINS(price, r',\d{2}$') 
@@ -38,23 +34,37 @@ WITH t1 AS (
                 WHEN REGEXP_CONTAINS(price, r'\.\d{2}$') 
                     THEN REGEXP_REPLACE(price, r"[,\']", '')
             END
-        AS NUMERIC) AS price
-        ,quantity
+        AS NUMERIC) AS price,
+        quantity
     FROM t1
+),
+t3 AS (
+    -- Aggregate duplicate product_id trong cùng order
+    SELECT
+        user_id_db,
+        email_address,
+        ip,
+        date_id,
+        store_id,
+        product_id,
+        order_id,
+        order_ts_utc,
+        currency,
+        price,
+        SUM(quantity) AS quantity,  -- tổng quantity
+        SUM(COALESCE(price,0) * SAFE_CAST(quantity AS NUMERIC)) AS revenue  -- tổng revenue
+    FROM t2
+    GROUP BY
+        user_id_db,
+        email_address,
+        ip,
+        date_id,
+        store_id,
+        product_id,
+        order_id,
+        order_ts_utc,
+        currency,
+        price
 )
-SELECT
-    user_id_db
-    ,email_address
-    ,ip
-    ,date_id
-    ,store_id
-    ,product_id
-    ,option_label
-    ,value_label
-    ,order_id
-    ,order_ts_utc
-    ,currency
-    ,price
-    ,quantity
-    ,COALESCE(price,0) * COALESCE(quantity,0) AS revenue
-FROM t2
+SELECT *
+FROM t3
