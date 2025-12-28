026ad1a2c73b93ed5c7edfa3b19d1210c9210953
q13111997 <quannguyenhong22@gmail.com>
2025-12-28 22:55:24 +0700

    commit

--- models/staging/staging_glamira/stg_glamira_sales_order.sql
+++ models/staging/staging_glamira/stg_glamira_sales_order.sql
@@ -14,18 +14,9 @@ WITH t1 AS (
     FROM {{ source('glamira_raw', 'summary') }}
     CROSS JOIN UNNEST(cart_products) AS cp
     WHERE collection = 'checkout_success'
-),
-t2 AS (
+), t2 AS (
     SELECT
-        user_id_db,
-        email_address,
-        ip,
-        date_id,
-        store_id,
-        product_id,
-        order_id,
-        order_ts_utc,
-        currency,
+        *,
         CAST(
             CASE 
                 WHEN TRIM(price) = '' THEN NULL 
@@ -34,37 +25,59 @@ t2 AS (
                 WHEN REGEXP_CONTAINS(price, r'\.\d{2}$') 
                     THEN REGEXP_REPLACE(price, r"[,\']", '')
             END
-        AS NUMERIC) AS price,
-        quantity
+        AS NUMERIC) AS price_num
     FROM t1
-),
-t3 AS (
-    -- Aggregate duplicate product_id trong cùng order
+), customer_per_order AS (
     SELECT
+        order_id,
         user_id_db,
         email_address,
         ip,
-        date_id,
         store_id,
-        product_id,
-        order_id,
+        date_id,
         order_ts_utc,
+        ROW_NUMBER() OVER (
+            PARTITION BY order_id
+            ORDER BY order_ts_utc
+        ) AS rn
+    FROM t2
+), order_customer AS (
+    SELECT
+        order_id,
+        user_id_db,
+        email_address,
+        ip,
+        store_id,
+        date_id,
+        order_ts_utc
+    FROM customer_per_order
+    WHERE rn = 1
+), order_product AS (
+    SELECT
+        order_id,
+        product_id,
         currency,
-        price,
-        SUM(quantity) AS quantity,  -- tổng quantity
-        SUM(COALESCE(price,0) * SAFE_CAST(quantity AS NUMERIC)) AS revenue  -- tổng revenue
+        SUM(quantity) AS quantity,
+        SUM(COALESCE(price_num, 0) * quantity) AS revenue,
+        SUM(COALESCE(price_num, 0) * quantity) / SUM(quantity) AS price
     FROM t2
     GROUP BY
-        user_id_db,
-        email_address,
-        ip,
-        date_id,
-        store_id,
-        product_id,
         order_id,
-        order_ts_utc,
-        currency,
-        price
+        product_id,
+        currency
 )
-SELECT *
-FROM t3
+SELECT
+    oc.user_id_db,
+    oc.email_address,
+    oc.ip,
+    oc.date_id,
+    oc.store_id,
+    oc.order_id,
+    oc.order_ts_utc,
+    op.product_id,
+    op.currency,
+    op.price,
+    op.quantity,
+    op.revenue
+FROM order_product op
+JOIN order_customer oc USING (order_id)
